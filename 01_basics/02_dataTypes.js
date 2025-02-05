864b621a5723d6e8f78baa8edf54bf5ee16dd3e9
janki-43 <janki4541@gmail.com>
2025-02-05 10:55:30 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using node.js, not browser
+console.log(3 + 3) // code readability should be high
+console.log("Janaki")
+
+let name = "Janaki"     //String data type
+let age = 21 // Number
+let isLoggedIn = false //Boolean 
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => stand alone value (not undefined its empty)
+// undefined => value is not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof "Janaki");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
