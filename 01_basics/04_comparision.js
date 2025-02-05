e1df2f1e94b57e9df21d68150dec5a14ddefb15a
janki-43 <janki4541@gmail.com>
2025-02-05 11:36:58 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// basic conversion
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// when same data type is not compared
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//Nan or 0 so avoid
+
+// console.log(null > 0); //f
+// console.log(null == 0); //f
+// console.log(null >= 0); //t
+// all false
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// === strict check
+console.log("2" === 2);
