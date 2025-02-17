943969d38ad0d48a67d8dc5984cac66f7d194531
janki-43 <janki4541@gmail.com>
2025-02-17 20:51:51 +0000

    while and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+//node 05_iterations/two.js
+// while
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+// let myArray = ['flash', 'batman', 'superman']
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+//do while 
+let score = 1//11
+do {
+    console.log(`Score is ${score}`);
+    score = score + 1
+} while (score<= 10);
